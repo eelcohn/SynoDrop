cc20b3b900d3dafcee15134d9e2c71c53383dd75
Synology <synology@synology.localdomain>
2018-01-15 08:27:39 +0100

    Eggdrop v1.8.3rc2 package for Synology DSM

---
+++ local.mk
@@ -0,0 +1,8 @@
+PUBLISH_URL=https://github.com/eelcohn/SynoDrop/releases/
+PUBLISH_API_KEY=
+MAINTAINER?=eelcohn
+MAINTAINER_URL=https://github.com/eelcohn/SynoDrop/
+DISTRIBUTOR=Eggheads
+DISTRIBUTOR_URL=https://www.eggheads.org
+REPORT_URL=https://github.com/eelcohn/SynoDrop/issues/
+DEFAULT_TC=5.2
